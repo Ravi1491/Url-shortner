6840b559f0d69318df0e7a2db8dcfbe89b6e9f85
Ravi Singh <ravi149185@gmail.com>
2023-11-14 22:38:46 +0530

    Add @nestjs/swagger and swagger-ui-express packages, and implement Swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,15 @@ async function bootstrap() {
   });
 
   app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('URL Shortener API')
+    .setDescription('List of all the APIs of URL Shortener')
+    .setVersion('1.0')
+    .addTag('url-shortener')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
   console.log('Server is running on http://localhost:3000');
