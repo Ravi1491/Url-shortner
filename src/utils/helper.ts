6023e97d1e9854dca4a7a8cc105ef9510215774a
Ravi Singh <ravi149185@gmail.com>
2024-11-20 12:38:51 +0530

    Adds utility functions for value validation
    Implements `isNilOrEmpty` and `isPresent` functions
    to streamline value checks using lodash utilities.
    
    Enhances code readability and reduces redundancy
    in value validation logic.

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -1,3 +1,5 @@
+import { isEmpty, isNil, isNull, isNaN, isUndefined } from 'lodash';
+
 export const getSanitizedUrl = (url: string) => {
   if (url.startsWith('http://')) {
     url = url.replace('http://', 'https://');
@@ -9,3 +11,12 @@ export const getSanitizedUrl = (url: string) => {
 
   return url;
 };
+
+export const isNilOrEmpty = (value: any) =>
+  isNil(value) ||
+  isEmpty(value) ||
+  isNull(value) ||
+  isNaN(value) ||
+  isUndefined(value);
+
+export const isPresent = (value: any) => !isNilOrEmpty(value);
